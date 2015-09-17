4f7c1490c7275753163824c0229053323f9fc657
Vinh Luong <vinh@uptake.io>
2015-09-17 14:50:42 -0500

    add iPython Notebook tutorial

--- Computing/Python/PythonInstallPackages.sh
+++ Computing/Python/PythonInstallPackages.sh
@@ -13,14 +13,16 @@ conda update scikit-learn
 # Install packages from PyPI (Python Package Index)
 pip install --upgrade pip
 
-pip install GGPlot
-pip install GraphViz
-pip install NeuroLab
+pip install --upgrade Bokeh
+pip install --upgrade GGPlot
+pip install --upgrade GraphViz
+pip install --upgrade Jupyter
+pip install --upgrade NeuroLab
 
 pip uninstall PyParsing   # to install an older version compatible with PyDot (see Stack Overflow thread:
 # http://stackoverflow.com/questions/15951748/pydot-and-graphviz-error-couldnt-import-dot-parser-loading-of-dot-files-will)
 pip install -Iv https://pypi.python.org/packages/source/p/pyparsing/pyparsing-1.5.7.tar.gz#md5=9be0fcdcc595199c646ab317c1d9a709
 pip install PyDot
 
-pip install Theano
-pip install Keras   # depends on Theano
+pip install --upgrade Theano
+pip install --upgrade Keras   # depends on Theano

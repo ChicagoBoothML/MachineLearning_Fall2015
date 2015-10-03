28145ae616af3e1cbd44b1b7666a1200375f6a93
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-03 10:34:31 -0500

    update RInstallPackages.R script

--- Computing/R/RInstallPackages.R
+++ Computing/R/RInstallPackages.R
@@ -1,14 +1,19 @@
 # The following will install necessary R packages for this course
 install.packages(c(
+  'bitops',
+  'caTools',
   'caret',
   'data.table',
   'devtools',
   'ggplot2',
+  'htmltools',
   'kknn',
+  'knitr',
   'knitr',
   'markdown',
   'MASS',
   'RCurl',
-  'rmarkdown'),
+  'rmarkdown',
+  'yaml'),
   dependencies = TRUE,
   repos="http://cran.rstudio.com")

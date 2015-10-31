e58aa7aa1857b8ce79abf475d8f6d732f817bbab
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-31 17:30:12 -0500

    add MovieLens (20 million) data parsers

--- Programming Scripts/MovieLens Movie Recommendation/Python/ParseData.py
+++ Programming Scripts/MovieLens Movie Recommendation/Python/ParseData.py
@@ -10,7 +10,7 @@ def parse_movielens_20m_data(
     movies = read_csv(join(data_path, 'movies.csv'))
 
     NB_RATINGS_FILES = 10
-    printflush('Parsing %i Ratings files:' % NB_RATINGS_FILES, end=' ')
+    printflush('Parsing %i Ratings Files:' % NB_RATINGS_FILES, end=' ')
     ratings = []
     for i in range(NB_RATINGS_FILES):
         ratings_file_name = 'ratings%02d.csv' % (i + 1)
---
+++ Programming Scripts/MovieLens Movie Recommendation/R/ParseData.R
@@ -0,0 +1,38 @@
+parse_movielens_20m_data <- function(
+  data_path='https://raw.githubusercontent.com/ChicagoBoothML/DATA___MovieLens___20M/master') {
+
+  library(data.table)
+  
+  movies <- fread(file.path(data_path, 'movies.csv'))
+  
+  NB_RATINGS_FILES = 10
+  
+  cat('Parsing ', NB_RATINGS_FILES, ' Ratings Files: ', sep='')
+  ratings <- data.table(
+    userId=integer(),
+    movieId=integer(),
+    rating=numeric(),
+    timestamp=character())
+  for (i in 1 : NB_RATINGS_FILES) {
+    ratings_file_name <- paste('ratings', formatC(i, width=2, flag=0), '.csv', sep='')
+    r <- fread(file.path(data_path, ratings_file_name),
+               colClasses=c(
+                 userId='integer',
+                 movieId='integer',
+                 rating='numeric',
+                 timestamp='character'),
+               stringsAsFactors=FALSE)
+    ratings = rbind(ratings, r)
+    cat(ratings_file_name, ', ')
+  }
+  cat('\n')
+    
+  list(movies=movies, ratings=ratings)
+}
+
+
+
+
+
+
+

49feb1301b9901ac98a1a1c54b31363ec588b8f6
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-10-31 16:17:41 -0500

    add MovieLens data parser in Python

---
+++ Programming Scripts/MovieLens Movie Recommendation/Python/ParseData.py
@@ -0,0 +1,22 @@
+from pandas import concat, read_csv
+from os.path import join
+
+from ChicagoBoothML_Helpy.Print import printflush
+
+
+def parse_movielens_20m_data(
+        data_path='https://raw.githubusercontent.com/ChicagoBoothML/DATA___MovieLens___20M/master'):
+
+    movies = read_csv(join(data_path, 'movies.csv'))
+
+    NB_RATINGS_FILES = 10
+    printflush('Parsing %i Ratings files:' % NB_RATINGS_FILES, end=' ')
+    ratings = []
+    for i in range(NB_RATINGS_FILES):
+        ratings_file_name = 'ratings%02d.csv' % (i + 1)
+        ratings.append(read_csv(join(data_path, ratings_file_name)))
+        printflush(ratings_file_name, end=', ')
+    printflush()
+    ratings = concat(ratings, ignore_index=True)
+
+    return dict(movies=movies, ratings=ratings)
